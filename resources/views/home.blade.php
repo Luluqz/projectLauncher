2a19bc45d1fa6c659d03e5343009f68dce076a54
Lucas Dmnt <lulu@MacBook-Pro-de-Lucas.local>
2017-02-06 02:09:41 +0100

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,7 +92,7 @@
         @foreach ($projects as $key => $project)
             <div class="col-md-4 col-sm-2">
                 <div class="img-project" style="background-image:url('http://lorempixel.com/600/400/')">
-                    <span class="catName">{{ $category[$key]->name }}</span>
+                    <span class="catName"><i class="fa fa-flag-o" aria-hidden="true"></i>{{ $category[$key]->name }}</span>
                 </div>
                 <div class="desc-project">
                     <h6><span>{{ $project->title }}</span></h6>
